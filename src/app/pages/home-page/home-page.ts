0157e9970a54c489cad6dc8099ac6756d3bc27dd
Ivan <ivanmorenolopez03@gmail.com>
2026-05-09 10:53:33 +0200

    eliminado innecesario

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { HeaderMain } from '../../layout/header-main/header';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink, HeaderMain],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })  
