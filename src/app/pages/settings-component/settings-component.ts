eb69a9d2d76fb7d33b3d16e04e8204748f74d442
Ivan <ivanmorenolopez03@gmail.com>
2026-05-23 09:45:42 +0200

    a

--- src/app/pages/settings-component/settings-component.ts
+++ src/app/pages/settings-component/settings-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-settings-component',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 })
 export class SettingsComponent {
 
-
+  resolucionWidth = signal(window.innerWidth);
 }
