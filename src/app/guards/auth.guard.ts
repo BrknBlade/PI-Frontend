137aeb1a7e8aee9346a09e89dbc1fdb3611c4abc
BrknBlade <frankoriven@gmail.com>
2026-05-25 14:16:34 +0200

    Solved auth guard failed to authenticate non admin users

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,11 +3,15 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth-service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.userID()) {
+    if(route.url[0].path == 'admin' && authService.user().role !== 1 && authService.user().role !== 2) {
+      return router.createUrlTree(['/']);
+    }
+
     return true;
   }
 
