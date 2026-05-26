b5a115371eb5a01afeafbdf48b41814062a4dd17
cristiann05 <ayalasanchezcristian2000@gmail.com>
2026-05-26 20:20:22 +0200

    probando con vercel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  BASE_URL : 'http://localhost:8000',
-  //BASE_URL: 'https://pi-backend-production-5f1c.up.railway.app'
+  BASE_URL: 'http://localhost:8000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  BASE_URL: 'https://pi-backend-production-f925.up.railway.app',
+};
