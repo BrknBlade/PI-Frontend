c6ad76468b3e29ceaa8d2bed612ff0bc02b03c2e
cristiann05 <ayalasanchezcristian2000@gmail.com>
2026-05-26 21:30:49 +0200

    arreglando problemas de compativilidad con backend

--- src/app/services/auth/auth-service.ts
+++ src/app/services/auth/auth-service.ts
@@ -3,30 +3,29 @@ import { Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { switchMap, tap, catchError, of } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
   user = signal<any>(null);
   userID = signal<any>(null);
 
   login(credentials: Object) {
     return this.http.get<any>(`${environment.BASE_URL}/sanctum/csrf-cookie`).pipe(
       switchMap(() => {
-        return this.http.post<any>(`${environment.BASE_URL}/api/login`, credentials)
+        return this.http.post<any>(`${environment.BASE_URL}/api/login`, credentials);
       }),
       tap((response) => {
-        console.log(response)
+        console.log(response);
         if (response?.user_id) {
-          //this.user.set(response.user);
           this.userID.set(response.user_id);
-        } else{
+          localStorage.setItem('auth_token', response.token);
+        } else {
           throw new Error(response?.message ?? 'Login fallido');
         }
       }),
-      switchMap(() => this.loadCurrentUser())
+      switchMap(() => this.loadCurrentUser()),
     );
   }
 
@@ -39,16 +38,17 @@ export class AuthService {
       catchError(() => {
         this.user.set(null);
         return of(null);
-      })
+      }),
     );
   }
 
   logout() {
-    return this.http.post(`${environment.BASE_URL}/api/logout`, {}).pipe(
-      tap(() => {
-        this.user.set(null);
-        this.userID.set(null);
-      })
-    );
-  }
+  return this.http.post(`${environment.BASE_URL}/api/logout`, {}).pipe(
+    tap(() => {
+      this.user.set(null);
+      this.userID.set(null);
+      localStorage.removeItem('auth_token');
+    })
+  );
+}
 }
